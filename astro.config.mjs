ecc097448d59a21f181e41a17d0d4c6f4f9bcd11
hamzamalik <hm2974022@gmail.com>
2024-11-11 21:25:38 +0500

    Set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://github.com/malikhamza88/countdown-site.git",
+  base: "/countdown-site/",
+  output: "static",
 });
